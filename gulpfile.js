d12a45d7c037870bc2c2ce9f548472e3974aca78
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-05-16 16:58:28 +0200

    Added a dependency on gulp and created a default gulp task for restarting server on js changes.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by babbarshaer on 2015-05-16.
+ */
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+
+
+gulp.task('default', function(){
+
+    nodemon({
+        script:'server.js',
+        ext:'js',
+        env:{
+            PORT: 9000
+        },
+        ignore: ['./node_modules/**']
+    })
+
+});
