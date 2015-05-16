95061d7fd8e1241b68f100722e847b28ba948ba4
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-05-16 19:28:15 +0200

    Added support for routes and implemented route for the about page.

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,18 @@
 
     app.config(["$logProvider", "$routeProvider", function($logProvider, $routeProvider){
         $logProvider.debugEnabled(true);
+
+        $routeProvider
+            //.when("/", {
+            //    templateUrl:'',
+            //    controller:'',
+            //    controllerAs:''
+            //})
+            .when("/about",{
+                templateUrl:'/app/templates/about.html'
+            })
+            .otherwise("/")
+
     }]);
 
 }());
