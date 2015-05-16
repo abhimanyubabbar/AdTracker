b9e26851dd2df69f20ca1f59a79b45574b8872ff
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-05-16 17:41:27 +0200

    Added code for fetching raw test data over a new route.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Main Express Application.
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var adDataFile = './server/data/ads.json';
+
 var app = express();
 
 
@@ -12,6 +15,17 @@ app.get("/", function(req, resp){
     resp.render('index');
 });
 
+app.get("/ads", function(req, resp){
+   resp.json(getAdData());
+});
+
+function getAdData(){
+    var data = fs.readFileSync(adDataFile);
+    return JSON.parse(data);
+}
+
+
+// PORT DEFINITION AND APP LISTENING.
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Ad Tracker Server Listening on: " + port);
